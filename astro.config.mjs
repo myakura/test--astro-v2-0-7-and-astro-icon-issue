745cdf8259ba3b5b0c68236c9867e84e42d3f617
Masataka Yakura <masataka.yakura@gmail.com>
2023-02-20 17:12:33 +0900

    add config and action file for deploying to GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://myakura.github.io",
+  base: "/test--astro-v2-0-7-and-astro-icon-issue",
+});
